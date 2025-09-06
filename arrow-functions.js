1adfb8e3aaee3200d90b54e46219642f51f7ab70
Unit786PK <muhammadasadali19@gmail.com>
2025-09-06 23:33:18 +0500

    Starting

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,5 +1,18 @@
+//classic function
 function add(num1, num2){
     return num1 + num2;
 }
 
 console.log(add(1,2));
+
+// Anonymous Function
+(function(num1, num2){
+    return num1 + num2;
+})();
+
+// Arrow Functions
+
+((num1, num2)=>{
+    return num1 + num2;
+})();
+(()=>num1 + num2)();
